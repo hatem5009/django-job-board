d271b504110913f7f8bf75fed3bc5b95822b76b6
Hatem Besbes <hatem5009@gmail.com>
2020-08-02 19:06:09 +0100

    slugify

--- job/models.py
+++ job/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 JOB_TYPE = (
     ('Full Time', 'Full Time'),
     ('Part Time', 'Part Time'),
 )
 # Create your models here.
+
+def image_upload(instance, filename):
+    imagename, extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id, extension)
+
+
 class Job(models.Model): # table in the database
 
     title = models.CharField(max_length=100) # colonne
@@ -16,8 +23,16 @@ class Job(models.Model): # table in the database
     salary = models.IntegerField(default=0)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     experience = models.IntegerField(default=0)
+    pictures = models.ImageField(upload_to=image_upload)
 
+    slug = models.SlugField(blank=True, null=True)
 
+    #codifier le url dans le browser
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job, self).save(*args,**kwargs)
+
+    #changer le titre de la page
     def __str__(self):
         return self.title
 
--- job/views.py
+++ job/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-from .models import Job
+from .models import Job, Category
+from django.core.paginator import Paginator
+
 # Create your views here.   
 
 def job_list(request):
     job_list = Job.objects.all()
-    context = {'jobs': job_list}
+    
+    paginator = Paginator(job_list, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    context = {'jobs': page_obj}
     return render(request, 'job/job_list.html', context)
     
-def job_detail(request, id):
-    job_detail = Job.objects.get(id=id)
+def job_detail(request, slug):
+    job_detail = Job.objects.get(slug=slug)
     context = {'job' : job_detail}
     return render(request, 'job/job_detail.html', context)
+
+def category_list(request):
+    category_list = Category.objects.all()
+    context = {'category' : category_list}
+    return render(request, 'job/category_list.html', context)
